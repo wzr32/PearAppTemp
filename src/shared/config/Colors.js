6583294759d618d3cf1ac8b5dc7159f71c97e2ff
Rubén  Padilla <rubenpadilla@MacBook-Pro-de-Ruben.local>
2022-12-07 23:56:00 -0400

    colors variables added

---
+++ src/shared/config/Colors.js
@@ -0,0 +1,6 @@
+export const LIGHT_GREEN_COLOR = "#10bc99";
+export const MEDIUM_GREEN_COLOR = "#134b45";
+export const DARK_GREEN_COLOR = "#013d37";
+
+export const WHITE_BG_COLOR = "#FFFFFF";
+export const GREY_BG_COLOR = "#F9F9F9";
