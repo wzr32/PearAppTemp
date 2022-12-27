546f26f647d783650a56dab3ec75b02819345cff
AbelinCoin <abelg896@gmail.com>
2022-12-27 01:34:32 -0400

    filters

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -4,6 +4,8 @@ import Layout from '../shared/layout/Layout';
 import HomeView from '../modules/home/HomeView';
 import AboutView from '../modules/about/AboutView';
 import FaqsView from '../modules/faqs/FaqsView';
+import FiltersView from '../modules/filters/FiltersView';
+import ProvidersView from '../modules/providers/ProvidersView';
 
 const Router = () => {
   return (
@@ -12,6 +14,8 @@ const Router = () => {
         <Route element={<HomeView />} path="/" />
         <Route element={<AboutView />} path="/about-us" />
         <Route element={<FaqsView />} path="/faqs" />
+        <Route element={<FiltersView />} path="/filters" />
+        <Route element={<ProvidersView />} path="/providers" />
       </Route>
     </Routes>  
   );
