9a2aec86efb4751e79c16d9ff5c9ef9507874702
Ruben Padilla <alejandro010492@gmail.com>
2023-02-07 21:28:03 -0400

    fix GoodbyeConflicts path

--- src/modules/home/components/GoodbyeConflicts.jsx
+++ src/modules/home/components/GoodbyeConflicts.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { Box, Container, Grid, Typography } from '@mui/material';
-import { DARK_GREEN_COLOR, LIGHT_GREEN_COLOR } from '../../../shared/config/Colors';
-import GBPhone from '../../../shared/assets/Landing_Phone.svg';
+import React from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+import {
+  DARK_GREEN_COLOR,
+  LIGHT_GREEN_COLOR,
+} from "../../../shared/config/Colors";
+import GBPhone from "../../../shared/assets/Landing_phone.svg";
 
 const GoodbyeConflicts = () => {
   return (
@@ -10,22 +13,51 @@ const GoodbyeConflicts = () => {
         <Grid container spacing={2} alignItems="center">
           <Grid item md={4}>
             <Box sx={{ width: "100%", height: "100%" }}>
-              <img src={GBPhone} alt="about_hand" className='landing-herobanner__image' />
+              <img
+                src={GBPhone}
+                alt="about_hand"
+                className="landing-herobanner__image"
+              />
             </Box>
           </Grid>
           <Grid item md={8}>
-            <Typography variant="h3" fontWeight="bold" textAlign="center" color={LIGHT_GREEN_COLOR} maxWidth="480px" margin={"0 auto .5rem auto"}>Goodbye Conflicts of Interest</Typography>
-            <Typography variant="h5" fontWeight="bold" textAlign="center" color={DARK_GREEN_COLOR}> Better Results </Typography>
-            <Typography variant="body1" component="span" display="flex" justifyContent="center" textAlign="center" maxWidth="420px" margin={"1rem auto 0 auto"}>
-              Our proprietary algorithm will present you the results
-              that best match your criteria, not the ones that will
-              pay us more commission.
+            <Typography
+              variant="h3"
+              fontWeight="bold"
+              textAlign="center"
+              color={LIGHT_GREEN_COLOR}
+              maxWidth="480px"
+              margin={"0 auto .5rem auto"}
+            >
+              Goodbye Conflicts of Interest
+            </Typography>
+            <Typography
+              variant="h5"
+              fontWeight="bold"
+              textAlign="center"
+              color={DARK_GREEN_COLOR}
+            >
+              {" "}
+              Better Results{" "}
+            </Typography>
+            <Typography
+              variant="body1"
+              component="span"
+              display="flex"
+              justifyContent="center"
+              textAlign="center"
+              maxWidth="420px"
+              margin={"1rem auto 0 auto"}
+            >
+              Our proprietary algorithm will present you the results that best
+              match your criteria, not the ones that will pay us more
+              commission.
             </Typography>
           </Grid>
         </Grid>
       </Box>
     </Container>
-  )
-}
+  );
+};
 
 export default GoodbyeConflicts;
